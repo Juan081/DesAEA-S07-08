a05d90a70ef29689d571a6aefedd81884e97f54d
Juan081 <jdiegotp22@gmail.com>
2023-10-21 23:28:51 -0500

    first commit

---
+++ Business1/BProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data1;
+using Entety2;
+namespace Business1
+{
+    public class BProduct
+    {
+        public List<Product> GetByName(string Name)
+        {
+           
+            DProduct data = new DProduct();
+            var products = data.Get();
+            var result= products.Where(x=>x.name == Name).ToList();
+            //lambda          
+
+            return result;
+        }
+        public List<Product> Get()
+        {
+            
+
+            DProduct data = new DProduct();
+            var products = data.Get();
+            
+            return products;
+        }
+    }
+}
---
+++ Data1/DProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entety2;
+
+namespace Data1
+{
+    public class DProduct
+    {
+        private readonly string connectionString = "Data Source=LAB1504-28\\SQLEXPRESS;Initial Catalog=FacturaDB;User ID=admin;Password=admin";
+
+        public List<Product> Get()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("listarProductos", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure; 
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                name = reader["Name"].ToString(),
+                                price = (decimal)reader["Price"],
+                                stock = (int)reader["Stock"],
+                                active = (bool)reader["Active"]
+                            };
+                            products.Add(product);
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
